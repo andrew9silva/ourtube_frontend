3776c089b59241a885f7c13a37d8005c8a53bb74
andrewsilva9 <andrewsilva9@gmail.com>
2020-06-01 14:58:05 -0600

    added code to index.js an index.html

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-console.log("in index.js");
+const url = 'http://localhost:3000/api/v1/videos'
+
+document.addEventListener('DOMContentLoaded', () => {
+    fetch(url)
+    .then(repsonse => response.json())
+    .then(videos => {
+        console.log(videos);
+    })
+})
