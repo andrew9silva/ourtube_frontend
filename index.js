fb7994b0e92d85ebdc883861c71e81f32f0fd0b4
andrewsilva9 <andrewsilva9@gmail.com>
2020-06-01 15:30:43 -0600

    added to index.html and index.js

--- index.js
+++ index.js
@@ -1,9 +1,24 @@
 const url = 'http://localhost:3000/api/v1/videos'
 
 document.addEventListener('DOMContentLoaded', () => {
-    fetch(url)
-    .then(repsonse => response.json())
-    .then(videos => {
-        console.log(videos);
-    })
+    getVideos()
 })
+
+function getVideos() {
+    fetch(url)
+    .then(response => response.json())
+    .then(videos => {
+        videos.data.forEach(video => {
+            const videoMarkup = `
+            <div data-id=${video.id}>
+            <video
+            // src=${video.attributes.url}
+            // height="200" width="250"
+            <h3>${video.attributes.title}</h3>
+            <p>${video.attributes.description}</p>
+            </div>
+            <br>`
+            document.querySelector('#video-container').innerHTML += videoMarkup
+        })
+    })
+}
